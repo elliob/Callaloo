2991f4026c8c3fe1ff6b1164c0d7fae28750a22c
elliob <elliob06@gmail.com>
2026-04-14 17:19:07 -0400

    Initial Commit

---
+++ Callaloo/CallalooApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallalooApp.swift
+//  Callaloo
+//
+//  Created by Bruce Elliott on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CallalooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
